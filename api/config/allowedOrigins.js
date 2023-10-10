d3af84763ab4b2b348f408e4ffa1fd2d14ec6668
nikos <nikos@gmail.com>
2023-10-11 01:02:23 +0300

    Vercel

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -3,7 +3,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5000',
-    'http://127.0.0.1:27017'
+    'http://127.0.0.1:27017',
+    'https://my-currency-calculator-app.vercel.app/'
 ]
   
 module.exports = allowedOrigins
